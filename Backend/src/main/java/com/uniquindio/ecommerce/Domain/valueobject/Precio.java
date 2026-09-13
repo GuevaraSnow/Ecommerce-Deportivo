939521f98944854432f304789fd725bc12a3743e
Santiago Guevara <santiguevar420@gmail.com>
2026-09-12 23:18:31 -0500

    fix: corregir validación y tipos del record Precio

--- Backend/src/main/java/com/uniquindio/ecommerce/Domain/valueobject/Precio.java
+++ Backend/src/main/java/com/uniquindio/ecommerce/Domain/valueobject/Precio.java
@@ -1,9 +1,10 @@
 package com.uniquindio.ecommerce.Domain.valueobject;
 
-public record Precio(duuble monto.String moneda) {
-    public Precio{
-        if (monto<0){
-            throw new
+public record Precio(double monto, String moneda) {
+
+    public Precio {
+        if (monto < 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo");
         }
     }
 }
